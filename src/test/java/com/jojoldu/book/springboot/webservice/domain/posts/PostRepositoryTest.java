bb2b5d75806b0ab19bcae20419801f997dcdd41a
gdaw3440 <gdaw3440@naver.com>
2021-09-13 11:22:24 +0900

    Postman as .http

---
+++ src/test/java/com/jojoldu/book/springboot/webservice/domain/posts/PostRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.jojoldu.book.springboot.webservice.domain.posts;
+
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        postsRepository.save(Posts.builder()
+                .title("테스트 게시글")
+                .content("테스트 본문")
+                .author("jojoldu@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle(), is("테스트 게시글"));
+        assertThat(posts.getContent(), is("테스트 본문"));
+    }
+}
