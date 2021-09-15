61a3ae2968ee99aee3ef5cf84762660c46f3adec
Zephyr <gdaw3440@naver.com>
2021-09-15 20:17:51 +0900

    hello, helloDto 테스트

--- src/main/java/com/jojoldu/book/springboot/webservice/domain/posts/PostsRepository.java
+++
@@ -1,6 +0,0 @@
-package com.jojoldu.book.springboot.webservice.domain.posts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostsRepository extends JpaRepository<Posts, Long> {
-}
